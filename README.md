a2d46e8b1adc3e98749efc01b0bd7827614c4d98
Christoffer Back <cback@cback.remote.csb>
2022-05-20 14:47:26 +0200

    initial

---
+++ README.md
@@ -0,0 +1,7 @@
+Endpoints - 
+'/' = Show added data
+'/POST' = POST data
+
+Runs with debug=true 
+Host is set to 0.0.0.0 to be compatible with container environment. 
+Port is set to 8080
---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+metrics = []
+
+@app.route('/')
+def get_data():
+    return jsonify(metrics)
+
+@app.route('/POST', methods=['POST'])
+def post_resp():
+    metrics.append(request.get_json())
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
