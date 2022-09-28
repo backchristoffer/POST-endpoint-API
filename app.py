8a300e0fc7980b0fa2472c20561cd58f36620cd1
Christoffer Back <backchristoffer@hotmail.com>
2022-09-28 18:20:06 +0200

    added pv_test

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, jsonify, request
 import os
 
+app = Flask(__name__)
+metrics = []
+
 #Create path for pv
 pv_path = '/data'
 if os.path.exists(pv_path) == True:
     os.mkdir(pv_path)
-
-app = Flask(__name__)
-metrics = []
 
 @app.route('/')
 def get_data():
@@ -16,9 +16,13 @@ def get_data():
 @app.route('/POST', methods=['POST'])
 def post_resp():
     metrics.append(request.get_json())
-    with open(os.path.join('/data','metrics.json'), "w") as file1:
-      file1.write(metrics)
     return '', 200
+
+@app.route('/PVTEST', methods=['GET'])
+def pv_test():
+    with open(os.path.join('/data','metrics.txt'), "w+") as file1:
+        file1.write("This is a pv test!")
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
